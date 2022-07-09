f6c9b476f86374125c55ca970808198bef7e9f4e
mogmog17 <108569805+mogmog17@users.noreply.github.com>
2022-07-09 13:27:55 +0000

    [Update]bookのeditページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,25 @@
 class BooksController < ApplicationController
-  def new
+  def index
+    @book = Book.new
+    @books = Book.all
   end
 
-  def index
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def show
   end
 
   def edit
+    @book = book.find(params[:id])
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
